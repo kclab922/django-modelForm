66eb53794a00132b889c382b9454f0948fde55c4
HaeunChoi <kclab922@gmail.com>
2023-11-03 09:32:06 +0900

    모델링 & 마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Aritcle(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.TextField()
+    # add(추가)가 되었을 때의 시간을 저장
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 추가와 관계없이 현재시간을 저장 
+    updated_at = models.DateTimeField(auto_now=True)
